49916e071fbebe4cc4daa1952de38db77e768247
Veronika Timakova <tymnik@ukr.net>
2024-01-20 21:23:25 +0200

    before bugfixes

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,100 @@
-import contactsService from "../services/contactsServices.js";
+import * as contactsService from "../services/contactsServices.js";
+import {
+  createContactSchema,
+  updateContactSchema,
+} from "../schemas/contactsSchemas.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res) => {
+  try {
+    const contacts = await contactsService.listContacts();
+    res.json(contacts);
+  } catch (error) {
+    console.error("Error fetching all contacts:", error);
+    res.status(500).send("Internal Server Error");
+  }
+};
 
-export const getContactById = (req, res) => {};
+export const getContactById = async (req, res) => {
+  const { id } = req.params;
 
-export const deleteContact = (req, res) => {};
+  try {
+    const contact = await contactsService.getContactById(id);
 
-export const createContact = (req, res) => {};
+    if (contact) {
+      res.json(contact);
+    } else {
+      res.status(404).json({ message: "Not found" });
+    }
+  } catch (error) {
+    console.error("Error fetching contact by ID:", error);
+    res.status(500).send("Internal Server Error");
+  }
+};
 
-export const updateContact = (req, res) => {};
+export const deleteContact = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedContact = await contactsService.removeContact(id);
+
+    if (deletedContact) {
+      res.json(deletedContact);
+    } else {
+      res.status(404).json({ message: "Not found" });
+    }
+  } catch (error) {
+    console.error("Error deleting contact:", error);
+    res.status(500).send("Internal Server Error");
+  }
+};
+
+export const createContact = async (req, res) => {
+  const { body } = req;
+
+  try {
+    const { error } = createContactSchema.validate(body);
+
+    if (error) {
+      return res.status(400).json({ message: error.message });
+    }
+
+    const newContact = await contactsService.addContact(body);
+
+    res.status(201).json(newContact);
+  } catch (error) {
+    console.error("Error creating contact:", error);
+    res.status(500).send("Internal Server Error");
+  }
+};
+
+export const updateContact = async (req, res) => {
+  const { id } = req.params;
+  const { body } = req;
+
+  try {
+    const { error } = updateContactSchema.validate(body);
+
+    if (error) {
+      return res.status(400).json({ message: error.message });
+    }
+
+    if (!Object.keys(body).length) {
+      return res
+        .status(400)
+        .json({ message: "Body must have at least one field" });
+    }
+
+    const existingContact = await contactsService.getContactById(id);
+
+    if (!existingContact) {
+      return res.status(404).json({ message: "Not found" });
+    }
+
+    const updatedContact = await contactsService.updateContact(id, body);
+
+    res.json(updatedContact);
+  } catch (error) {
+    console.error("Error updating contact:", error);
+    res.status(500).send("Internal Server Error");
+  }
+};
---
+++ services/contactsServices.js
@@ -0,0 +1,56 @@
+import fs from "fs/promises";
+import { fileURLToPath } from "url";
+import { join, dirname } from "path";
+import { nanoid } from "nanoid";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const contactsPath = join(__dirname, "db", "contacts.json");
+
+async function readContactsFile() {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+}
+
+async function writeContactsFile(contacts) {
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+}
+
+async function listContacts() {
+  const contacts = await readContactsFile();
+  return contacts;
+}
+
+async function getContactById(id) {
+  const contacts = await readContactsFile();
+  return contacts.find((contact) => contact.id === id) || null;
+}
+
+async function removeContact(id) {
+  const contacts = await readContactsFile();
+  const index = contacts.findIndex((contact) => contact.id === id);
+
+  if (index !== -1) {
+    const [removedContact] = contacts.splice(index, 1);
+    await writeContactsFile(contacts);
+    return removedContact;
+  }
+
+  return null;
+}
+
+async function addContact(data) {
+  const contacts = await readContactsFile();
+  const newContact = {
+    id: nanoid(),
+    ...data,
+  };
+
+  contacts.push(newContact);
+  await writeContactsFile(contacts);
+
+  return newContact;
+}
+
+export { listContacts, getContactById, removeContact, addContact };
